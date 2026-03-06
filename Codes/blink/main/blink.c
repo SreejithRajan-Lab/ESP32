cdec2d530d6fb5373977935cc02173276abe4ad0
Sreejith Rajan <sreejithkriisc@gmail.com>
2026-03-06 19:48:22 +0530

    GPIO Documentation added

--- Codes/blink/main/blink.c
+++ Codes/blink/main/blink.c
@@ -1,20 +1,22 @@
-#include "driver/gpio.h"
+#include <stdio.h>
+#include <driver/gpio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define LED GPIO_NUM_2   // Change if your board uses a different LED pin
+#define led_pin GPIO_NUM_2
 
 void app_main(void)
 {
-    gpio_reset_pin(LED);
-    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(led_pin); // reset gpio2
+    gpio_set_direction(led_pin,GPIO_MODE_OUTPUT);
+while(1)
+{
+    gpio_set_level(led_pin,1);  //Pin 2 High
+    vTaskDelay(pdMS_TO_TICKS(500));  // Command to provide 1s delay
 
-    while (1)
-    {
-        gpio_set_level(LED, 1);   // LED ON
-        vTaskDelay(pdMS_TO_TICKS(1000));
+    gpio_set_level(led_pin,0); // Pin 2  Low
+    vTaskDelay(pdMS_TO_TICKS(500));  // Command to provide 1s delay
 
-        gpio_set_level(LED, 0);   // LED OFF
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+}
+
 }
