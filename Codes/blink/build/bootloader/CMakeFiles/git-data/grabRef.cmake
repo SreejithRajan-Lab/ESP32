d1f8c7c1998fc8cab1f0edf95497b0fa9b3e9140
Sreejith Rajan <sreejithkriisc@gmail.com>
2026-03-25 18:11:15 +0530

    adc stack added

--- Codes/blink/build/bootloader/CMakeFiles/git-data/grabRef.cmake
+++ Codes/blink/build/bootloader/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/sreej/ESP32/Codes/blink/build/bootloader/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/sreej/esp/v5.5.2/esp-idf/.git")
+set(GIT_DIR "C:/esp/v5.5.3/esp-idf/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
